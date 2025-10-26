9ea7095b52a5416926bf069a0755514410da45e9
Dilanka240 <it24610796@my.sliit.lk>
2025-10-26 16:40:44 +0530

    Delete backend directory

--- backend/src/main/java/com/example/GiftCraft_BackEnd/Entity/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.GiftCraft_BackEnd.Entity;
-
-public enum OrderStatus {
-    Pending, Processing, Shipped, Delivered, Cancelled
-}
--- backend/src/main/java/com/example/GiftCraft_BackEnd/Entity/PaymentStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.GiftCraft_BackEnd.Entity;
-
-public enum PaymentStatus {
-    PENDING, VERIFIED, FAILED, REFUNDED
-}
-
